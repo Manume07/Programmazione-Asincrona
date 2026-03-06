9b7a9a1fcf595bc213e4933b61f39e535d7ff9e9
Manuel Campana <MANUEL.CAMPANA@local.ittcesena.it>
2026-03-06 09:52:56 +0100

    Finire

---
+++ ProgrammazioneAsincrona/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProgrammazioneAsincrona
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Random rnd = new Random();
+        char[] lettere = new char[26] {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+       private void Rolling()
+        {
+
+        }
+
+        private void btnStampa_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnScelta_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+    }
+}
